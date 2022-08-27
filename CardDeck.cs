e34a7af48bf15e6dbe2f30aa76a62e2c027882ba
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 20:09:06 +0530

    Added CardDeck class

---
+++ CardDeck.cs
@@ -0,0 +1,20 @@
+﻿namespace DeckOfCards
+{
+    public class CardDeck
+    {
+        public string[] Suit = { "Clubs", "Daimonds", "Hearts", "Spade" };
+        public string[] Rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        public string[] Deck = new string[52];
+
+        public CardDeck()
+        {
+            var counter = 0;
+            foreach (var s in Suit)
+                foreach (var r in Rank)
+                {
+                    Deck[counter] = String.Format("{0} of {1}", r, s);
+                    counter++;
+                }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DeckOfCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var UI = new CardsToPlayer();
+            UI.AddPlayer();
+            UI.DistributeCards();
+            UI.SeeCards();
+        }
+    }
+}
